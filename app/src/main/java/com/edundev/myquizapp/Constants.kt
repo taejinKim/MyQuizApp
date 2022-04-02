df392f93848f730ea0641e2b0e8f27efd368849f
manowar72 <manowar72@gmail.com>
2022-04-03 07:17:57 +0900

    Initial commit

---
+++ app/src/main/java/com/edundev/myquizapp/Constants.kt
@@ -0,0 +1,8 @@
+package com.edundev.myquizapp
+
+object Constants {
+    const val USER_NAME : String = "user_name"
+    const val TOTAL_QUESTIONS : String = "total_questions"
+    const val CORRECT_ANSWERS : String = "correct_answers"
+
+}
